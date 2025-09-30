ddbe8887019b77f3c51f0b5c3b46a5865002df24
Vik <vikoliveira.dev@gmail.com>
2025-09-30 10:16:04 -0300

    Conexão com o banco de dados

--- db.py
+++ db.py
@@ -1,2 +1,23 @@
-import psycopg2
+import psycopg2 as pg
+from dotenv import load_dotenv
+import os
 #pip install dotenv
+
+
+params = {
+    "dbname": os.getenv("DB_NAME"),
+    "dbuser": os.getenv("DB_USER"),
+    "dbpassword": os.getenv("DB_PASSWORD"),
+    "dbhost": os.getenv("DB_HOST"),
+    "dbport": os.getenv("DB_PORT"),
+}
+
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
+
